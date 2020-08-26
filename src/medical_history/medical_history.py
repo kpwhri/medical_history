b12e06c62e8f4a242f62eb4e930918f595cd1c9b
dcronkite <dcronkite@gmail.com>
2020-08-26 13:50:16 -0700

    Added list of output flags

--- src/medical_history/medical_history.py
+++ src/medical_history/medical_history.py
@@ -5,6 +5,16 @@ from regexify import Pattern
 
 class MedicalHistoryFlag(Enum):
     UNKNOWN = 0
+    NEGATED = 1
+    PERSONAL = 2
+    DEGREE1 = 3
+    DEGREE1_NEG = 4
+    DEGREE2 = 5
+    DEGREE2_NEG = 6
+    OTHER = 7
+    OTHER_NEG = 8
+    FAMILY = 9
+    FAMILY_NEG = 10
 
 
 def is_negated(text):
